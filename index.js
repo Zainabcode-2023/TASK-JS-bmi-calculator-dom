faa2af29e707da5f1712918dbaf84d9194f88186
Zainabcode-2023 <anasalwajod1996@gmail.com>
2023-05-11 18:17:12 +0300

    solving challenge 2

--- index.js
+++ index.js
@@ -8,4 +8,20 @@ function calculateBMI() {
   console.table({ age, weight, height, male, female });
 
   // You are going to write the code here before the curly braces 👇🏻
+  height = height / 100;
+  let bmi = weight / (height * height);
+  bmi = bmi.toFixed(2);
+
+  alert("your bmi is " + bmi);
+
+  if (bmi <= 18.5) {
+    console.log("underweight");
+  } else if (bmi <= 18.5 || bmi >= 25) {
+    console.log("healthy weight");
+  } else if (bmi >= 25 || bmi <= 30) {
+    console.log("overweight");
+  } else if (bmi >= 30) {
+    console.log("above obesity");
+  }
+  alert(" the bmi state is:" + bmi);
 }
